a303c41a59d54c56c30a7430513c3e25d3496c77
Nagendra6743 <relanginagendrababu00@gmail.com>
2025-05-21 11:27:58 +0530

    echo command usage

---
+++ 01-Helloworld.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "Welcome to Shell scripting" # echo is used to print the content on the screen
---
+++ 02-Conversation_2_persons.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Trump:: Hey Musk, How are you?"
+echo "Musk:: Hello Trump, I am fine. How are you doing"
+echo "Trump: I am fine too. What's up?"
+echo "Musk:: Nohting, just going to Mars now, will you come?"
+echo "Trump:: Sorry, you carry on! I will come once you come back"
+
+# echo is used to print the content on the screen
