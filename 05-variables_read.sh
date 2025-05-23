d182165041c4bed95c02c3f2d55c74f7fee674cf
Nagendra6743 <relanginagendrababu00@gmail.com>
2025-05-23 19:41:41 +0530

    read - reads user input

---
+++ 05-variables_read.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo  "Enter your PIN number::   "
+read -s PIN        # here ""read" command takes input from user in (-s)silent(Don't show while providing pin)
+echo "Your PIN number is:: $PIN"
