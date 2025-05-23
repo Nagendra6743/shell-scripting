6e5474db295e82fab55bdb17b0103c87fa8fd591
Nagendra6743 <relanginagendrababu00@gmail.com>
2025-05-23 19:29:30 +0530

    Variables_default passing arguments

---
+++ 04-variables_default_Arguments.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$1:: Hey $2, How are you?"
+echo "$2:: Hello $1, I am fine. How are you doing"
+echo "$1: I am fine too. What's up?"
+echo "$2:: Nohting, just going to Mars now, will you come?"
+echo "$1:: Sorry, you carry on! I will come once you come back"
