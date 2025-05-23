d397eaeaeb60604ebce67f124543ede417af87d6
Nagendra6743 <relanginagendrababu00@gmail.com>
2025-05-23 19:22:14 +0530

    variables assigned with values

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=Nagendra    #there should be no space before and after equals to (==)
+PERSON2=Sai
+
+echo "$PERSON1 :: Hi $PERSON2, How are you??"
+echo "$PERSON2 :: Hello $PERSON1, I am doing great. How about you??"
+
+
+#$ git add 03-variables.sh
+#warning: in the working copy of '03-variables.sh', LF will be replaced by CRLF the next time Git touches it
