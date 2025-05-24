0805a92dcf97ac16ebf6ccef0f70fee175a9fe83
Nagendra6743 <relanginagendrababu00@gmail.com>
2025-05-24 11:21:23 +0530

    Installation checks

--- 10-install.sh
+++ 10-install.sh
@@ -5,6 +5,7 @@ userid=$(id -u)
     if [ $userid -ne 0 ]
     then
         echo "ERROR::Please run with root access"
+        exit
     else
         echo "The user is running with root access, please proceed with the installation"
     fi
