1315bcbe610497fb90e9e4a52e64ecd53d663ef4
Nagendra6743 <relanginagendrababu00@gmail.com>
2025-05-24 17:34:59 +0530

    Installation checks

--- 13-colors.sh
+++ 13-colors.sh
@@ -2,9 +2,9 @@
 
 USERID=(id -u)
 
-if [ $USERID -ne 0]
+if [ $USERID -ne 0 ]
     then 
-    echo " Please help to run the script with root access"
+    echo "Please help to run the script with root access"
     exit 1
 else 
     echo "You are running with root access..."
