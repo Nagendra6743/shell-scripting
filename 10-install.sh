6171684cb2893b619f1afcedeba1ae09bd448d36
Nagendra6743 <relanginagendrababu00@gmail.com>
2025-05-24 15:32:40 +0530

    Installation checks

--- 10-install.sh
+++ 10-install.sh
@@ -14,7 +14,7 @@ userid=$(id -u)
         if [ $? -ne 0 ]
         then
             echo "installing the mysql package ...."
-            dnf install mysqlffkgk -y
+            dnf install mysql -y
             if [ $? -eq 0 ] 
             then
             echo "MYSQL package installed SUCCESSFULLY"
