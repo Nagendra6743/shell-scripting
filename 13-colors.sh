fea82e244fe9d68f59d7a61e75ef085a5e3b6a88
Nagendra6743 <relanginagendrababu00@gmail.com>
2025-05-24 17:48:42 +0530

    Installation checks

--- 13-colors.sh
+++ 13-colors.sh
@@ -9,3 +9,21 @@ if [ $USERID -ne 0 ]
 else 
     echo -e "\e[32mYou are running with root access...\e[0m"
 fi
+
+dnf list installed mysql
+    if [ $? -ne 0 ]
+        then 
+        dnf install mysql -y
+        if [ $? -eq 0 ]
+        then 
+        echo -e "Insalling mysql is...\e[32mSUCCESS\e[0m"
+        else
+        echo -e "Insalling mysql is...\e[31mFAILED\e[0m"
+        exit 1
+        fi
+    else
+        echo -e "\e[0mMYSQL already been Installed ...Nothing to do\e[0m"
+    fi
+       
+
+
