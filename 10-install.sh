e2835e504970c653dcea9019ef88fd8e1a7eb6d4
Nagendra6743 <relanginagendrababu00@gmail.com>
2025-05-24 11:32:46 +0530

    Installation checks

--- 10-install.sh
+++ 10-install.sh
@@ -10,10 +10,21 @@ userid=$(id -u)
         echo "The user is running with root access, please proceed with the installation"
     fi
 
+    dnf list installed mysql
+        if [ $? -eq 0]
+        then 
+          echo "mysql already installed!!!"
+          exit 1
+          else 
+          echo "mysql not installed, please proceed with mysql installation"
+          fi
+
     dnf install mysql -y
+        echo "mysql installation is in progress..."
           if [ $? -eq 0 ]
           then 
           echo "mysql installed successfully!!!"
           else 
           echo "ERROR::mysql installation failed"
           fi
+    
