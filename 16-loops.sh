43b7a5cf58affdc1875d89df42f62b14c99af7a2
Nagendra6743 <relanginagendrababu00@gmail.com>
2025-05-25 15:01:04 +0530

    loops

--- 16-loops.sh
+++ 16-loops.sh
@@ -7,8 +7,8 @@ W="\e[0m"
 
 if [ $USERID -ne 0 ];
     then
-    echo "$R ERROR:Please help to run with root access to proceed with further action$W"
+    echo -e "$R ERROR:Please help to run with root access to proceed with further action $W"
     exit 1
     else
-    echo "$G You are running with root access and you can now install application$W"
+    echo -e "$G You are running with root access and you can now install application$W"
 fi
