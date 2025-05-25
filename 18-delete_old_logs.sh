d8df318562d02b46c5b2d725a94b2e8c19cbe960
Nagendra6743 <relanginagendrababu00@gmail.com>
2025-05-25 17:49:03 +0530

    loops

--- 18-delete_old_logs.sh
+++ 18-delete_old_logs.sh
@@ -8,7 +8,7 @@ FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 while IFS= read -r filepath  #IFS=: Sets the Internal Field Separator (IFS) to an empty value, ensuring leading/trailing spaces are preserved.
 do
     echo "Deleting file: $filepath"
-done <<< $FILES_TO_DELETE
+done < $FILES_TO_DELETE
 
 echo "Script executed successfully"
 
